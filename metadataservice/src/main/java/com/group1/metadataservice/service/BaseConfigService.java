bebcce69f8ab414a556f571a676c970ce3954f6e
Nguyễn Võ Minh Châu <nguyenvominhchau165@gmail.com>
2026-03-19 08:35:57 +0700

    Up code metadata service

--- metadataservice/src/main/java/com/group1/metadataservice/service/BaseConfigService.java
+++
@@ -1,9 +0,0 @@
-package com.group1.metadataservice.service;
-
-import com.group1.metadataservice.model.entity.BaseConfig.BaseConfig;
-
-public interface BaseConfigService {
-    void update(String key, String value);
-
-    BaseConfig getByKey(String key);
-}
--- metadataservice/src/main/java/com/group1/metadataservice/service/EffectiveConfigService.java
+++
@@ -1,7 +0,0 @@
-package com.group1.metadataservice.service;
-
-import com.group1.metadataservice.model.dto.EffectiveConfigDTO;
-
-public interface EffectiveConfigService {
-    EffectiveConfigDTO getEffectiveConfig(String key, String regionCode);
-}
