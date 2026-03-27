bd6039072a6711d0a8a5e6d63339f4831b504291
Huynh Bao <huynhthienbao294294@gmail.com>
2026-03-27 20:43:59 +0700

    .

--- metadataservice/src/main/java/com/group1/app/shift/controller/AttendanceReportController.java
+++ metadataservice/src/main/java/com/group1/app/shift/controller/AttendanceReportController.java
@@ -20,7 +20,7 @@ import java.time.ZoneId;
 import java.util.List;
 
 @RestController
-@RequestMapping("/shift-service/attendance-reports")
+@RequestMapping("/api/shift-service/attendance-reports")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AttendanceReportController {
@@ -50,7 +50,6 @@ public class AttendanceReportController {
             @RequestParam(required = false)
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
             LocalDate date,
-
             @RequestParam(required = false) String branchId
     ) {
         if (date == null) {
